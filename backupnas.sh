7b95efada9b4a80ba13a4e8b5dca59c6b2445399
chiveturkey <andersen.eric@gmail.com>
2019-11-03 14:43:11 -0600

    Break NAS backup variables out into config file.

--- backupnas.sh
+++ backupnas.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# TODO: Break this out into a config file.
-volumes=(volume1 volume2)
+# Read in config file.
+while IFS='=' read -r name value
+do
+  export $name=${value//\'/}
+done < backups.config
+
+# Convert volumes string to array.
+IFS=',' read -ra volumes <<< "$volumes"
 
 echo "Backing up NAS volumes."
 echo
@@ -10,11 +16,11 @@ date
 echo
 
 for volume in ${volumes[*]}; do
-  if [ -f /nas/$volume/SEMAPHORE.txt ]; then
+  if [ -f $nasdir/$volume/SEMAPHORE.txt ]; then
     echo "Backing up $volume on NAS to backup server."
     echo "========================================"
     echo
-    rsync -avh --delete /nas/$volume/ /backups/$volume/
+    rsync -avh --delete $nasdir/$volume/ $backupdir/$volume/
     echo
   else
     echo "SEMAPHORE.txt missing.  Is NAS volume $volume mounted?"
