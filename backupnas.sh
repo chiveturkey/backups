a132df677265b2df5b4f53449193392643bb33be
chiveturkey <andersen.eric@gmail.com>
2019-12-02 11:52:38 -0600

    Add logic to handle relative path for backups.config.

--- backupnas.sh
+++ backupnas.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Change directory to the directory the application is running in, to allow the relative path to
+# backups.config.
+#
+# HACKTAG: Using a relative path to read config files is error prone.
+# See http://mywiki.wooledge.org/BashFAQ/028
+#
+# In particular, "[i]t is important to realize that in the general case, this problem has no
+# solution. Any approach you might have heard of, and any approach that will be detailed below, has
+# flaws and will only work in specific cases."  Since this script is for personal use, and since I
+# use it in a fairly limited fashion, I think this is an acceptable risk.  For more general use on
+# diverse systems, it would be preferable to use an absolute path for backups.config.
+cd "${BASH_SOURCE%/*}/" || exit
+
 # Read in config file.
 while IFS='=' read -r name value
 do
