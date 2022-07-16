0543bf3333726998751b3945bb25d8eed559c163
chiveturkey <38541318+chiveturkey@users.noreply.github.com>
2022-07-16 16:12:51 -0500

    Update encryption/decryption to break file into parts.  Also, general cleanup.

--- b2backup.py
+++ b2backup.py
@@ -2,7 +2,6 @@
 
 '''Module providing backups to B2.'''
 import os
-# import pprint
 import re
 import tarfile
 from datetime import date
@@ -18,7 +17,9 @@ THISMONTH = '{:%Y%m}01'.format(date.today())
 # TODO: This needs to be read in at run time.  Making it a constant for now.
 SECRET_KEY = b'abcdefghijklmnopqrstuvwxyz012345'
 BACKUP_DIRECTORY = '.'
+ENCRYPTED_FILE_PART_SIZE = 1024
 DEBUG = False
+
 
 # Methods
 
@@ -49,7 +50,7 @@ def list_files_matching(match_pattern, backup_directory=BACKUP_DIRECTORY):
     # TODO: Consider adding error checking on presence of legit 'backup_directory', and/or add
     # reasonable default that is set if config file value doesn't exist.  Or fail if config value
     # doesn't exist...?
-    for filename in os.listdir(backup_directory):
+    for filename in sorted(os.listdir(backup_directory)):
         if re.search(match_pattern, filename):
             print(filename)
     print()
@@ -62,20 +63,23 @@ def list_local_archives(volumes=VOLUMES):
 def encrypt_archives(volumes=VOLUMES,
                      backup_directory=BACKUP_DIRECTORY,
                      thismonth=THISMONTH,
+                     encrypted_file_part_size = ENCRYPTED_FILE_PART_SIZE,
                      key=SECRET_KEY):
-    '''Encrypt archives with PyNaCl.'''
+    '''Function encrypting archives with PyNaCl.  Output encrypted files of size encrypted_file_part_size.'''
     format_log('Encrypting volumes.')
     for volume in volumes.split(','):
         with open(f'{backup_directory}/{thismonth}-{volume}.tar.gz', 'rb') as volume_file:
-            # TODO: This is a naive first pass.  It reads the *entire* contents of the file at
-            # once.  If the file is large, it will fill up available RAM very quickly.
-            volume_contents = volume_file.read()
-            # volume_contents = b'This is some test text to encrypt.'
-            box = nacl.secret.SecretBox(key)
-            encrypted_volume_contents = box.encrypt(volume_contents)
-            # print(encrypted_volume_contents)
-            with open(f'{backup_directory}/{thismonth}-{volume}.tar.gz.enc', 'wb') as encrypted_volume_file:
-                encrypted_volume_file.write(encrypted_volume_contents)
+            volume_contents_part = b' '
+            part_number = 1
+            while volume_contents_part != b'':
+                volume_contents_part = volume_file.read(encrypted_file_part_size)
+                if volume_contents_part != b'':
+                    box = nacl.secret.SecretBox(key)
+                    encrypted_volume_contents_part = box.encrypt(volume_contents_part)
+                    with open(f'{backup_directory}/{thismonth}-{volume}.tar.gz.enc.part{part_number:03d}',
+                              'wb') as encrypted_volume_file_part:
+                        encrypted_volume_file_part.write(encrypted_volume_contents_part)
+                    part_number += 1
 
 def list_local_encrypted_archives(volumes=VOLUMES):
     '''Function listing local encrypted archives.'''
@@ -86,20 +90,20 @@ def decrypt_archives(volumes=VOLUMES,
                      backup_directory=BACKUP_DIRECTORY,
                      thismonth=THISMONTH,
                      key=SECRET_KEY):
-    '''Decrypt archives with PyNaCl.'''
+    '''Function decrypting archives with PyNaCl.  Input encrypted file parts, and output decrypted archive.'''
     format_log('Decrypting volumes.')
     for volume in volumes.split(','):
-        with open(f'{backup_directory}/{thismonth}-{volume}.tar.gz.enc', 'rb') as encrypted_volume_file:
-            # TODO: This is a naive first pass.  It reads the *entire* contents of the file at
-            # once.  If the file is large, it will fill up available RAM very quickly.
-            encrypted_volume_contents = encrypted_volume_file.read()
-            box = nacl.secret.SecretBox(key)
-            volume_contents = box.decrypt(encrypted_volume_contents)
-            with open(f'{backup_directory}/{thismonth}-{volume}.tar.gz', 'wb') as volume_file:
-                volume_file.write(volume_contents)
+        part_number = 1
+        while os.path.isfile(f'{backup_directory}/{thismonth}-{volume}.tar.gz.enc.part{part_number:03d}'):
+            with open(f'{backup_directory}/{thismonth}-{volume}.tar.gz.enc.part{part_number:03d}',
+                      'rb') as encrypted_volume_file_part:
+                encrypted_volume_contents_part = encrypted_volume_file_part.read()
+                box = nacl.secret.SecretBox(key)
+                volume_contents_part = box.decrypt(encrypted_volume_contents_part)
+                with open(f'{backup_directory}/{thismonth}-{volume}.tar.gz', 'ab') as volume_file:
+                    volume_file.write(volume_contents_part)
+            part_number += 1
 
-# def split_file():
-#     with open()
 
 # Main
 
